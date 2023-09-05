42ef8263d954f74822755334b907788d348e75f2
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-09-05 16:23:16 +0300

    Update Program.cs

--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,129 +1,107 @@
-using OOP;
-using System.Runtime.Intrinsics.X86;
-using System.Security.Cryptography.X509Certificates;
-using System.Threading.Tasks.Dataflow;
-
-public class Message
+using System.Drawing;
+using System.Threading.Channels;
+public class Group
 {
-    private readonly string _content;
-    private readonly string _author;
-    private readonly DateTime _time;
-    private int _likes;
-    public Message(string content, string author, DateTime time)
+    public List<string> liiked { get; } = new List<string>();
+    private readonly int _maxAmount;
+    public Group(int maxAmount)
     {
-        this._content = content;
-        this._author = author;
-        this._time = time;
+        _maxAmount = maxAmount;
     }
-    public int Likes { get => _likes; }
-    public DateTime Time { get => _time; }
-    public string Author { get => _author; }
-    public string Content { get => _content; }
-    public void AddLike()
+    public bool AddMember(string liik)
     {
-        _likes++;
+        if (liiked.Contains(liik)) return false;
+        if (liiked.Count >= _maxAmount) return false; ;
+        liiked.Add(liik);
+        return true;
     }
-    public double GetPopularity()
+    public int GetMembersCount()
     {
-        double elapsed = DateTime.Now.Subtract(this._time).TotalSeconds;
-        if (elapsed == 0)
+        return liiked.Count;
+    }
+    public bool HasMember(string member)
+    {
+        return liiked.Contains(member);
+    }
+    public class chel
+    {
+        private readonly string _firstName;
+        private readonly string _lastName;
+        private readonly int _age;
+        public chel(string firstName, string lastName,int age)
         {
-            return _likes;
+            this._firstName = firstName;
+            this._lastName = lastName;
+            this._age = age;
         }
-        return _likes / elapsed;
-    }
-    public void vivod()
-    {
-        Console.WriteLine(Author);
-        Console.WriteLine(Content);
-        Console.WriteLine(Time);
-        Console.WriteLine(GetPopularity());
-    }
-    public string popular(double first, double second)
-    {
-        string res = "";
-        if (first> second)
-        {
-            res = "First bolshe second";
-        }
-        else if (first < second)
-        {
-            res = "Second bolshe first";
-        }
-        return res;
-    }
-    public string GetPopylarityInfoN(List <Message> messages)
-    {
-        double popularity = 0;
-        for (int i = 0; i < messages.Count; i++)
-        {
-            if (messages[i].GetPopularity() > popularity)
-            {
-                popularity = messages[i].GetPopularity();
-            }
-        }
-        string res = "";
-        return res;
+        public int age { get => _age; }
+        public string firstName { get => _firstName; }
+        public string lastName { get => _lastName; }
 
+        public void vivod()
+        {
+            Console.WriteLine(firstName);
+            Console.WriteLine(lastName);
+            Console.WriteLine(age);
+        }
     }
-}
 public class Program
 {
     public static void Main()
     {
+        Random rndage = new Random();
+        int t = 1 * rndage.Next(0, 100);
+        chel m1 = new chel("Hello", "John", 23);
 
-        List<Message> messeges = new List<Message>();
-        Random rndauthor = new Random();
-        Random rndcontent = new Random();
-        string stroka = "Glass: ";
-        switch (rndcontent.Next(2))
+
+        Console.WriteLine("Kui palju inimesed: ");
+        int mutu = Convert.ToInt32(Console.ReadLine());
+        Group group = new Group(mutu);
+        string name = "";
+        for (int i = 0; i < mutu; i++)
         {
-            case 0:
-                stroka += " cold";
+            char theKey = Console.ReadKey().KeyChar;
+            if (theKey == 'y' || theKey == 'Y')
+            {
+                i = mutu;
                 break;
-            case 1:
-                stroka += " hot";
-                break;
+            }
+            else
+                {
+                string Rname = Console.ReadLine();
+                name = Rname;
+                Console.WriteLine(group.AddMember(name));
+                if (group.AddMember(name)==false)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Nimi lisanud :"+name);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else if (group.AddMember(name) == true)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Nimi ei õige:"+ name);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+            }
+
+            
         }
-        string strokaAuthor = "Autor";
-        switch (rndauthor.Next(2))
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(string.Join(", ", group.liiked));
+        Console.WriteLine(group.GetMembersCount());  // 3
+        Console.ForegroundColor = ConsoleColor.Gray;
+        Console.WriteLine("Kui palju inimesed: ");
+        int mutu1 = Convert.ToInt32(Console.ReadLine());
+        Group bookClub = new Group(mutu1);
+        for (int i = 0; i < mutu1; i++)
         {
-            case 0:
-                strokaAuthor += " Georg";
-                break;
-            case 1:
-                strokaAuthor += " Evgeni";
-                break;
+            Console.WriteLine("Nimi: ");
+            string name1 = Console.ReadLine();
+            bookClub.AddMember(name1);
         }
-        int t = -1 * rndauthor.Next(0, 100);
-        Console.Write("Author: ");
-        string auter = Console.ReadLine();
-        Console.Write("Content: ");
-        string conti = Console.ReadLine();
-        Message m1 = new Message("Hello", "John", DateTime.Now.AddDays(-10));
-        m1.AddLike();
-        m1.vivod();
-        Message m2 = new Message("Hi", "Mary", DateTime.Now.AddMinutes(-1));
-        m2.AddLike();
-        m2.vivod();
-        Message m3 = new Message(strokaAuthor.ToString(), stroka.ToString(), DateTime.Now.AddMinutes(t));
-        m3.AddLike();
-        m3.vivod();
-        Message m4 = new Message(conti.ToString(), auter.ToString(), DateTime.Now.AddMinutes(t));
-        m4.AddLike();
-        m4.vivod();
-        messeges.Add(m1);
-        messeges.Add(m2);
-        messeges.Add(m3);
-        messeges.Add(m4);
-        for (int i = 0; i < 1000; i++)
-        {
-            m2.AddLike();
-        }
-        Console.WriteLine(m1.popular(m1.GetPopularity(),m2.GetPopularity()));
-        List<Message> List = new List<Message>();
-        List.Add(m1);
-        List.Add(m2);
-        Console.WriteLine(m1.GetPopylarityInfoN(List));
+        Console.WriteLine(string.Join(", ", bookClub.liiked));  // Albert, Samantha, Robert, Roberta
+        Console.WriteLine(bookClub.HasMember("Albert"));  // True
+        Console.WriteLine(bookClub.HasMember("John"));  // False
     }
 }
