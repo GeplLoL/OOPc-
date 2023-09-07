6c755cc8b9e68c99faa95368be193e11ff718e38
Larmesdesang <85700200+GeplLoL@users.noreply.github.com>
2023-09-08 02:04:25 +0300

    Add files via upload

---
+++ OOP/GroupManage.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using static Group;
+
+public class GroupManager
+{
+    public static void ManageGroup()
+    {
+        Random rndage = new Random();
+        int t = 1 * rndage.Next(0, 100);
+        Console.WriteLine("Kui palju inimesed group: ");
+        int mutu = Convert.ToInt32(Console.ReadLine());
+        Group group = new Group(mutu);
+        string nameRL = "";
+        string nameRnd = "";
+        int age1 = 0;
+        Random rndB = new Random();
+        Group bookClub = new Group(mutu);
+        Group AnimeClub = new Group(mutu);
+        Group SportClub = new Group(mutu);
+        for (int i = 0; i < mutu; i++)
+        {
+            int Rnb = rndB.Next(0, 3);
+            Random rndPN = new Random();
+            int num = rndPN.Next(0, 5);
+            switch (num)
+            {
+                case 0:
+                    nameRnd = "Deniss";
+                    break;
+                case 1:
+                    nameRnd = "Roma";
+                    break;
+                case 2:
+                    nameRnd = "Kirill";
+                    break;
+                case 3:
+                    nameRnd = "Aleksander";
+                    break;
+                case 4:
+                    nameRnd = "Oleg";
+                    break;
+                case 5:
+                    nameRnd = "Martin";
+                    break;
+            }
+            Random rndPL = new Random();
+            int num1 = rndPL.Next(0, 5);
+            switch (num1)
+            {
+                case 0:
+                    nameRL = "Gorjunov";
+                    break;
+                case 1:
+                    nameRL = "Kulo4enko";
+                    break;
+                case 2:
+                    nameRL = "Obromovitch";
+                    break;
+                case 3:
+                    nameRL = "Miteev";
+                    break;
+                case 4:
+                    nameRL = "Kaluzkii";
+                    break;
+                case 5:
+                    nameRL = "Orkestrov";
+                    break;
+            }
+            Random rndPA = new Random();
+            age1 = rndPL.Next(5, 100);
+
+            chel man = new chel(nameRnd, nameRL, age1);
+
+            Console.WriteLine("Kui soovite täitmise lõpetada, vаjutage 'y' or no");
+            string ans = Console.ReadLine();
+            if (ans == "y")
+            {
+                char theKey = Console.ReadKey(true).KeyChar;
+                if (theKey == 'y' || theKey == 'Y' || ans =="y" || ans =="Y")
+                {
+                    i = mutu;
+                    Console.WriteLine(string.Join(", ", group.liiked));
+                    break;
+                }
+            }
+            if (group.AddMember(nameRnd, nameRL, age1) == true)
+            {
+                group.AddMember(nameRnd, nameRL, age1);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Andmed lisanud: " + nameRnd + " " + nameRL + " " + age1);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                if (Rnb==1)
+                {
+                    bookClub.AddMember(nameRnd, nameRL, age1);
+                }
+                else if (Rnb==2)
+                {
+                    AnimeClub.AddMember(nameRnd, nameRL, age1);
+                }
+                else
+                {
+                    SportClub.AddMember(nameRnd, nameRL, age1);
+                }
+            }
+            else if (group.AddMember(nameRnd, nameRL, age1) == false)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Andmed ei õige: " + nameRnd + " " + nameRL + " " + age1);
+                Console.ForegroundColor = ConsoleColor.Gray;
+            }
+        }
+        Console.WriteLine("All");
+        Console.WriteLine(string.Join(", ", group.liiked));
+        Console.WriteLine(group.GetMembersCount());
+        Console.WriteLine("Book club");
+        Console.WriteLine(string.Join(", ", bookClub.liiked));
+        Console.WriteLine("arve: ");
+        Console.WriteLine(bookClub.GetMembersCount());
+        Console.WriteLine("Sport club");
+        Console.WriteLine(string.Join(", ", SportClub.liiked));
+        Console.WriteLine("arve: ");
+        Console.WriteLine(SportClub.GetMembersCount());
+        Console.WriteLine("Anime club");
+        Console.WriteLine(string.Join(", ", AnimeClub.liiked));
+        Console.WriteLine("arve: ");
+        Console.WriteLine(AnimeClub.GetMembersCount());
+
+    }
+}
--- OOP/Program.cs
+++ OOP/Program.cs
@@ -1,4 +1,4 @@
-using System.Drawing;
+﻿using System.Drawing;
 using System.Threading.Channels;
 public class Group
 {
@@ -8,11 +8,12 @@ public class Group
     {
         _maxAmount = maxAmount;
     }
-    public bool AddMember(string liik)
+    public bool AddMember(string _firstName, string _lastName, int _age)
     {
-        if (liiked.Contains(liik)) return false;
-        if (liiked.Count >= _maxAmount) return false; ;
-        liiked.Add(liik);
+        string fullName= _firstName + " " + _lastName +" "+ _age;
+        if (liiked.Contains(fullName)) return false;
+        if (liiked.Count >= _maxAmount) return false;
+        liiked.Add(fullName);
         return true;
     }
     public int GetMembersCount()
@@ -28,7 +29,7 @@ public class Group
         private readonly string _firstName;
         private readonly string _lastName;
         private readonly int _age;
-        public chel(string firstName, string lastName,int age)
+        public chel(string firstName, string lastName, int age)
         {
             this._firstName = firstName;
             this._lastName = lastName;
@@ -45,63 +46,11 @@ public class Group
             Console.WriteLine(age);
         }
     }
-public class Program
-{
-    public static void Main()
+    public class Program
     {
-        Random rndage = new Random();
-        int t = 1 * rndage.Next(0, 100);
-        chel m1 = new chel("Hello", "John", 23);
-
-
-        Console.WriteLine("Kui palju inimesed: ");
-        int mutu = Convert.ToInt32(Console.ReadLine());
-        Group group = new Group(mutu);
-        string name = "";
-        for (int i = 0; i < mutu; i++)
+        public static void Main()
         {
-            char theKey = Console.ReadKey().KeyChar;
-            if (theKey == 'y' || theKey == 'Y')
-            {
-                i = mutu;
-                break;
-            }
-            else
-                {
-                string Rname = Console.ReadLine();
-                name = Rname;
-                Console.WriteLine(group.AddMember(name));
-                if (group.AddMember(name)==false)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine("Nimi lisanud :"+name);
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                }
-                else if (group.AddMember(name) == true)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Nimi ei õige:"+ name);
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                }
-            }
-
-            
+            GroupManager.ManageGroup();
         }
-        Console.ForegroundColor = ConsoleColor.Green;
-        Console.WriteLine(string.Join(", ", group.liiked));
-        Console.WriteLine(group.GetMembersCount());  // 3
-        Console.ForegroundColor = ConsoleColor.Gray;
-        Console.WriteLine("Kui palju inimesed: ");
-        int mutu1 = Convert.ToInt32(Console.ReadLine());
-        Group bookClub = new Group(mutu1);
-        for (int i = 0; i < mutu1; i++)
-        {
-            Console.WriteLine("Nimi: ");
-            string name1 = Console.ReadLine();
-            bookClub.AddMember(name1);
-        }
-        Console.WriteLine(string.Join(", ", bookClub.liiked));  // Albert, Samantha, Robert, Roberta
-        Console.WriteLine(bookClub.HasMember("Albert"));  // True
-        Console.WriteLine(bookClub.HasMember("John"));  // False
     }
 }
